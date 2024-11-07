c44d1295c15eb5aac711015c270bcdbd2d4d0918
David <david.berdiell@gft.com>
2024-11-07 13:13:35 +0100

    add coors

--- src/main/java/com/example/app/controller/CategoriaRestController.java
+++ src/main/java/com/example/app/controller/CategoriaRestController.java
@@ -9,6 +9,7 @@ import com.example.app.model.Categoria;
 import com.example.app.response.CategoriaResponseRest;
 import com.example.app.services.ICategoriaService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/v1")
 public class CategoriaRestController {
